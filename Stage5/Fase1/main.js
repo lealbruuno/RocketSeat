57e4d9fb3efb98063c13f8d14e0408ac336616f7
Bruno Leal <98700610+lealbruuno@users.noreply.github.com>
2023-02-22 20:05:42 -0300

    Add files via upload

---
+++ Stage5/Fase1/main.js
@@ -0,0 +1,37 @@
+//Variáveis
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+//Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+
+//Funções
+function handleTryClick(event) {
+  event.preventDefault();
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    togleScreen();
+    screen2.querySelector(
+      "h2"
+    ).innerText = `Acertou em ${xAttempts} tentativas`;
+  }
+
+  inputNumber.value = "";
+  xAttempts++;
+}
+
+function handleResetClick() {
+  togleScreen();
+  xAttempts = 1;
+}
+
+function togleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
