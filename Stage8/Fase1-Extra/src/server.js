4af1cecb08f1eeb86250e656d42b385ad972880a
Bruno Leal <98700610+lealbruuno@users.noreply.github.com>
2023-04-12 23:25:35 -0300

    Add files via upload

---
+++ Stage8/Fase1-Extra/src/server.js
@@ -0,0 +1,37 @@
+require("express-async-errors"); //Biblioteca para tratamento de exceções//
+const migrationsRun = require("./database/sqlite/migrations"); //Pacote do Banco de Dados//
+const AppError = require("./utils/AppError"); //Tratamento de exceções//
+
+const express = require("express");
+const routes = require("./routes/index");
+
+migrationsRun();
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+  //Verifica se o erro vem de uma instância (instanceof) de um AppError//
+  ///Erro gerado do lado do cliente/
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  ///Erro gerado do lado do servidor/
+  return response.status(500).json({
+    status: "error",
+    message: error.message,
+  });
+});
+
+const PORT = 5555;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
